25012c977dbb691a6ac29e95f9a43b528bd45492
user <wafaesadki2019@gmail.com>
2026-09-07 14:21:12 +0200

    Added Gemistry Attunement Theme java class

---
+++ src/main/java/com/danako/gemistry/core/GemistryAttunementTheme.java
@@ -0,0 +1,25 @@
+package com.danako.gemistry.core;
+
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.Enchantment;
+
+public enum GemistryAttunementTheme {
+    PYRIC(GemistryTags.ATTUNEMENT_CATALYSTS_RUBY, GemistryTags.ATTUNEMENT_PYRIC), BOREAL(GemistryTags.ATTUNEMENT_CATALYSTS_SAPPHIRE, GemistryTags.ATTUNEMENT_BOREAL), ABYSSAL(GemistryTags.ATTUNEMENT_CATALYSTS_AQUAMARINE, GemistryTags.ATTUNEMENT_ABYSSAL), MIASMIC(GemistryTags.ATTUNEMENT_CATALYSTS_AMBER, GemistryTags.ATTUNEMENT_MIASMIC);
+
+    private final TagKey<Item> catalystTag;
+    private final TagKey<Enchantment> themeTag;
+
+    GemistryAttunementTheme(TagKey<Item> catalystTag, TagKey<Enchantment> themeTag) {
+        this.catalystTag = catalystTag;
+        this.themeTag = themeTag;
+    }
+
+    public TagKey<Item> catalystTag() {
+        return this.catalystTag;
+    }
+
+    public TagKey<Enchantment> themeTag() {
+        return this.themeTag;
+    }
+}
